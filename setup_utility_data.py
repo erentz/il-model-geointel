2a7cd021a0de0c524abe76a5ffedf2c32da11b42
Malika El Haddi <35073679+malikaelhaddi@users.noreply.github.com>
2021-01-27 14:54:42 -0600

    Update setup_utility_data.py

--- setup_utility_data.py
+++ setup_utility_data.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 22 11:37:25 2021
-
-@author: malik
-"""
 
 # -*- coding: utf-8 -*-
 """
@@ -16,6 +10,8 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 import datetime
+import pandas as pd
+import numpy as np
 import ussolar.esri as uss
 from arcpy import env
 
@@ -34,9 +30,10 @@ manual_subs_kmz_id      =   '1_5mtkFe4VSPmnPanPU0XbV935NDHvE2q'
 il_iou                  =   r'C:\USS\United States Solar Corporation\Site Selection - Documents\Data\State\IL\Manual\IL_IOU\IL_IOU.shp'
 il_non_iou              =   r'C:\USS\United States Solar Corporation\Site Selection - Documents\Data\State\IL\Manual\IL_NON_IOU\IL_NON_IOU.shp'
 cap_screen_req_dir      =   r"C:\USS\United States Solar Corporation\IL - State Level Resources\{}\Hosting Capacity\Capacity Screen Points"
-
+cap_screen_res_dir      =   r'C:\USS\United States Solar Corporation\IL - State Level Resources\{}\Hosting Capacity\Capacity Screen Results'
 phi                     =   'DeleteTemp'
 
+cap_screen_table        =   "{}_CapacityScreen_Results"
 county_fc               =   "Political/IL_Counties"
 substations_fc          =   'UtilityData/IL_Substations'
 util_service_fc         =   'UtilityData/IL_ServiceAreas'
@@ -44,6 +41,7 @@ cap_screen_points_fc    =   "UtilityData/{0}_CapacityScreenPoints"
 
 util_dict               =   {'ComEd': 'ComEd',
                              'Ameren': 'Ameren'}
+
 
 def ImportKMZ(in_kmz, output_fc, fc_type):
     # Define Path Variables
@@ -59,6 +57,13 @@ def ImportKMZ(in_kmz, output_fc, fc_type):
     # Delete Intermediate
     arcpy.Delete_management(os.path.join(scratch_directory,scratch_gdb_name+".lyr"))
     arcpy.Delete_management(os.path.join(scratch_directory,scratch_gdb_name+".gdb"))
+
+def ExportDF2Arc(in_df, col_names, out_table):
+    x = np.array(np.rec.fromrecords(in_df.values))
+    x.dtype.names = tuple(col_names)
+    if arcpy.Exists(os.path.join(env.workspace, out_table)):
+        arcpy.Delete_management(os.path.join(env.workspace, out_table))
+    arcpy.da.NumPyArrayToTable(x, os.path.join(env.workspace, out_table))
 
 def ImportSpatialUtilityData():
     # Grab Manual Substations
@@ -94,11 +99,20 @@ def ImportSpatialUtilityData():
     print("***ImportSpatialUtilityData Finished!***")
     print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-
-# ---> Replace using output of the create_preapps script
+def UtilityMath(utility, in_df):
+    if utility == 'ComEd':
+      
+        in_df["HostingCapacity"] = ((((in_df["Thermal_capacity_rating_of_feeder_or_sub"]+ in_df["_15pct_peak_load_kW"])-in_df["Existing_DER_on_feeder_or_sub"])/1)*.001)
+ 
+    elif utility == 'Ameren':
+   
+       in_df["HostingCapacity"] = (((in_df["normal_rating_of_substation_MVA"]+ in_df["feeder_min_load_MVA"])-in_df["Existing_generation_on_circuit"])/1)
+       
+        
 def ImportCapacityScreenData(in_utility):
     # Assign Utility Specific output
     points_fc               =   cap_screen_points_fc.format(util_dict[in_utility])
+    cap_screen_results      =   cap_screen_table.format(util_dict[in_utility])
     # Put together a list of Cap Screen KMZs
     cap_screen_kmz          =   []
     for f in os.listdir(cap_screen_req_dir.format(util_dict[in_utility])):
@@ -132,7 +146,7 @@ def ImportCapacityScreenData(in_utility):
         arcpy.CalculateField_management(output_fc, "Lat", '!X!', "PYTHON_9.3")
         arcpy.AddField_management(output_fc, "Long", "STRING")
         arcpy.CalculateField_management(output_fc, "Long", '!Y!', "PYTHON_9.3")
-        arcpy.CalculateField_management(output_fc, "UID",'!Lat!+", "+!Long!', 'PYTHON_9.3')
+        arcpy.CalculateField_management(output_fc, "UID",'!Lat!+","+!Long!', 'PYTHON_9.3')
     # Merge all Cap Screens into one file
     arcpy.Merge_management(cap_screen_fc, points_fc)
     # Format Final Capacity Screen Request FC
@@ -141,13 +155,99 @@ def ImportCapacityScreenData(in_utility):
     for fc in cap_screen_fc:
         arcpy.Delete_management(fc)
         arcpy.Delete_management(fc+phi)
+    #get capacity screen result file list    
+    cap_screen_xlsx = []
+    for file in os.listdir(cap_screen_res_dir.format(in_utility)):
+        if file.endswith(".xlsx"):
+            cap_screen_xlsx.append(os.path.join((cap_screen_res_dir.format(in_utility)), file))
+   
+    #import all capacity screen result files
+    cap_screen_tables = []
+    for xlsx in cap_screen_xlsx:
+        xl = pd.ExcelFile(xlsx)
+        #create DataFrame
+        cap_screen_df = xl.parse(xl.sheet_names[0])
+        # Table name
+        out_table = in_utility + " " + os.path.basename(xlsx[:-5])
+        out_table = out_table.replace(" ", "_") 
+        ###Calculate hosting capacity based on utility specific variables
+        UtilityMath(in_utility, cap_screen_df)
+        #Create and assign Arc Compatible Column Names
+        if in_utility == 'ComEd':
+            ##make columns for Circuit & Substation which comed reports as same number
+            cap_screen_df['Substation'] = cap_screen_df['feeder_or_sub_number']
+            cap_screen_df['Circuit']    = cap_screen_df['feeder_or_sub_number']
+            col_dict = {
+            'CapScreenName': 'CapScreenName',
+            'Substation' : 'Substation',
+            'Circuit': 'Circuit',
+            'feeder_or_sub_number': 'Feeder',
+            'Thermal_capacity_rating_of_feeder_or_sub':'Substation_Cap_Rating_MVA',
+            'Thermal_capacity_rating_of_feeder_or_sub':'Feeder_Cap_Rating_MVA',
+            'Existing_DER_on_feeder_or_sub':'Existing_DG_MW',
+            'Pending_DER_on_feeder_or_sub':'Queue_MW',
+            'Available_capacity_on_feeder_or_sub':'Available_Sub_Cap_MW',
+            'Available_capacity_on_feeder_or_sub':'Available_Feeder_Cap_MW',
+            'Nominal_Voltage_at_sub': 'Distribution_Voltage_kv',
+            'Obvious_Hurdles':'Obvious_Hurdle',
+            'HostingCapacity':'HostingCapacity'
+            }
+            cap_screen_df.reindex(columns=col_dict)
+            cap_screen_df = cap_screen_df[col_dict.keys()]
+            cap_screen_df.rename(columns=col_dict, inplace=True) 
+            column_list = list(cap_screen_df)
+            ##convert units
+            for field in column_list[4:-3]:
+                cap_screen_df[field] = cap_screen_df[field]/(1000.0)
+        else:
+            ###Ameren Column Names
+            col_dict = { 
+            'CapScreenName': 'CapScreenName',   
+            'Substation':'Substation',
+            'POI_Ciruit': 'Circuit',
+            'Feeder': 'Feeder',
+            'normal_rating_of_substation_MVA':'Substation_Cap_Rating_MVA',
+            'normal_rating_of_POI_circuit_MVA':'Feeder_Cap_Rating_MVA',
+            'Existing_generation_on_circuit':'Existing_DG_MW',
+            'queued_generation_mW':'Queue_MW',
+            'available_substation_capacity_MVA':'Available_Sub_Cap_MW',
+            'available_circuit_capacity_MVA':'Available_Feeder_Cap_MW',
+            'substation_nominal_distribution_voltage':'Distribution_Voltage_kV',
+            'Obvious_Hurdles':'Obvious_Hurdle',
+            'HostingCapacity':'HostingCapacity'
+            }
+            cap_screen_df.reindex(columns=col_dict)
+            cap_screen_df = cap_screen_df[col_dict.keys()]
+            cap_screen_df.rename(columns=col_dict, inplace=True)
+        column_list = list(cap_screen_df)
+        cap_df = cap_screen_df[column_list]
+        ExportDF2Arc(cap_df, column_list, out_table.format(util_dict[in_utility]))
+        cap_screen_tables.append(out_table.format(util_dict[in_utility]))
+    if arcpy.Exists(cap_screen_results):
+        arcpy.Delete_management(cap_screen_results)
+    arcpy.Merge_management(cap_screen_tables, cap_screen_results)
+    fieldnames = []
+    fields = arcpy.ListFields(cap_screen_results)
+    for field in fields:
+        if not field.required:
+            fieldnames.append(field.name)
+    fieldnames.remove( "CapScreenName")
+    arcpy.JoinField_management(points_fc,"UID", cap_screen_results, "CapScreenName",fieldnames)
+    
+    for fc in cap_screen_fc:
+        arcpy.Delete_management(fc)
+        arcpy.Delete_management(fc+phi)
+    for table in cap_screen_tables:
+        arcpy.Delete_management(table)
+    print("***ImportCapacityScreenData Finished!***")
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        
 
 def Main():
     ImportSpatialUtilityData()
-    ImportCapacityScreenData('ComEd')
+    for util in util_dict.keys():
+        ImportCapacityScreenData(util)
     
-    ImportCapacityScreenData('Ameren')
-   
 try:
     drive
 except:
